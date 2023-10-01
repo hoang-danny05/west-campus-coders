2f50dc35b1f4879d7caab73a7f8e890f598518bf
hoang-danny05 <bajanboss132@gmail.com>
2023-10-01 16:50:15 -0700

    updated the trident to the smaller version; added event listeners

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log('Hello!');
 
+const image = document.getElementById("trident");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("true")
+            image.classList.add("up")
+        } else {
+            //for if you want it to replay
+            console.log("false")
+            image.classList.remove("up")
+        }
+    })
+})
 
 const animate = () => {
     const image = document.querySelectorAll("img.trident");
@@ -8,4 +21,6 @@ const animate = () => {
     }
 }
 
-animate();
+// animate();
+const elements = document.querySelectorAll("span.clubname-bottom");
+elements.forEach((element) => observer.observe(element))
