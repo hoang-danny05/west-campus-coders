2094807a1ce570164d357680d0dd920cac176a85
hoang-danny05 <bajanboss132@gmail.com>
2023-10-01 15:45:18 -0700

    formatted text and added trident to document

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello!');
+
+
+const animate = () => {
+    const image = document.querySelectorAll("img.trident");
+    for (const item of image) {
+        console.log("thing done");
+    }
+}
+
+animate();
